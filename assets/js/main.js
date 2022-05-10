2948c293fe457bf251609a1efd6286a810640b4e
cankayali <cankayali02@gmail.com>
2022-05-10 16:08:59 +0300

    header completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,40 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    
+    const navMenu = document.getElementById('nav-menu')
+
+    // When we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
@@ -13,10 +45,10 @@
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
 
-/* Link active featured */ 
+/* Link active featured */
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
